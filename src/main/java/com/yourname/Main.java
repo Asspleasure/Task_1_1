0a5e28ffeeb8cf05fe4972dee81c020b29419208
Valentin <wallpaperlowup@gmail.com>
2018-10-14 19:31:21 +0300

    dfd

---
+++ src/main/java/com/yourname/Main.java
@@ -0,0 +1,8 @@
+package com.yourname;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Valentin Getalo");
+        System.out.print("КВО НВК №100");
+    }
+}
